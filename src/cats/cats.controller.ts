57a9fa89d9dcbd3a41711ad5ca425f43c311a488
hnquy156 <hnquy.hbt@gmail.com>
2023-12-14 17:32:35 +0700

    Add custom exception

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CreateCatDto } from 'src/cats/dto/create-cat.dto';
 import { Cat } from 'src/cats/interfaces/cat.interface';
 import { CatsService } from 'src/cats/cats.service';
+import { ForbiddenException } from 'src/forbidden.exception';
 
 @Controller('cats')
 export class CatsController {
@@ -14,6 +15,7 @@ export class CatsController {
 
   @Get()
   async findAll(): Promise<Cat[]> {
+    throw new ForbiddenException();
     return this.catsService.findAll();
   }
 }

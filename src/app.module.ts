9856813a780a30c62b85d965933893e47cf87ae8
hnquy156 <hnquy.hbt@gmail.com>
2023-12-14 17:16:49 +0700

    Add class middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'cats', method: RequestMethod.POST }, 'cats/(.*)')
+      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+  }
+}

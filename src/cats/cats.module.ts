2d7c4db179cc2fd380e9fd27f9aeb98707ca82a4
hnquy156 <hnquy.hbt@gmail.com>
2023-12-14 17:49:44 +0700

    Add exception filter

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { AllExceptionsFilter } from 'src/http-exception.filter';
 
 @Module({
   controllers: [CatsController],
-  providers: [CatsService],
+  providers: [
+    CatsService,
+    { provide: 'APP_FILTER', useClass: AllExceptionsFilter },
+  ],
 })
 export class CatsModule {}

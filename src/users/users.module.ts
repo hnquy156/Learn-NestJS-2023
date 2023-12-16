516e8e08fb5a70d0d81e4f40658d06c6d591f1ff
hnquy156 <hnquy.hbt@gmail.com>
2023-12-16 16:31:15 +0700

    Add User Subcriber

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
+import { UserSubscriber } from './user.subcriber';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  providers: [UsersService, UserSubscriber],
   controllers: [UsersController],
 })
 export class UsersModule {}

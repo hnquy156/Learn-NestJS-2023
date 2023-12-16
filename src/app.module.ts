2b9e9f3f0e9910bc9b7259e46300590eed39030d
hnquy156 <hnquy.hbt@gmail.com>
2023-12-16 11:26:03 +0700

    Add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,27 @@ import {
 import { CatsModule } from './cats/cats.module';
 import { logger } from './logger.middleware';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'mssql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PW,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      retryAttempts: 1,
+      options: {
+        trustServerCertificate: true,
+        enableArithAbort: true,
+      },
+    }),
+    CatsModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
